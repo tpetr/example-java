5b52ded93906cc159bc9dfc72c861c8280b9c249
Tom Petr <trpetr@gmail.com>
2024-06-11 14:51:47 -0400

    Update Foo.java

--- com/semgrep/Foo.java
+++ com/semgrep/Foo.java
@@ -1,9 +1,15 @@
 package com.semgrep;
 
+import com.semgrep.Bar;
+
 class Foo {
   public static int VALUE = 42;
 
   public void testMethod() {
     // bla
   }
+
+  public static void main(String... args) {
+    System.out.println("Foo value: " + Foo.VALUE);
+    System.out.println("Var value: " + Bar.VALUE_PLUS_ONE);
 }
